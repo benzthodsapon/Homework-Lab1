60b3139f0ebaa8aef93c0d3581e15e95a70288da
benzthodsapon <benz_thodsapon@outlook.com>
2022-01-11 12:51:16 +0700

    Homework

---
+++ Homework1-2.js
@@ -0,0 +1,21 @@
+let x= 50;
+
+if ((x >= 80) && (x <= 100)) {
+    console.log("youßr grade is A");
+}
+
+else if ((x >= 70) && (x <= 79)) {
+    console.log("your grade is B");
+}
+
+else if ((x >= 60) && (x <= 69)) {
+    console.log("your grade is C");
+}
+
+else if ((x >= 50) && (x <= 59)) {
+    console.log("your grade is D");
+}
+
+else {
+    console.log("your grade is E");
+}
---
+++ Homework1-3.js
@@ -0,0 +1,13 @@
+let x= 3;
+
+if (x> 0) {
+
+    console.log ("x = positive");
+} else if (x < 0) {
+
+    console.log ("x = negative");
+
+} else {
+
+    console.log ("x = zero");
+}
